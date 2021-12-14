291d0315bf37026a526a166cb9ada5e2c0af41df
Anurag Singh <anuragdiago@gmail.com>
2021-12-14 15:15:41 +0530

    ui change in fav screen

--- lib/screens/favorites.dart
+++ lib/screens/favorites.dart
@@ -29,72 +29,92 @@ class _FavoritesState extends State<Favorites> {
           : ListView.builder(
               itemCount: favorites.length,
               itemBuilder: (context, index) {
-                return Container(
-                  height: 120,
-                  margin: EdgeInsets.all(5),
-                  padding: EdgeInsets.all(10),
-                  decoration: BoxDecoration(color: Colors.black),
-                  child: Row(
-                    children: [
-                      Image(
-                        image: NetworkImage("https://image.tmdb.org/t/p/w500" +
-                            favorites[index]["posterurl"]),
-                      ),
-                      SizedBox(
-                        width: 15,
-                      ),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Text(
-                              favorites[index]["name"],
-                              style: TextStyle(
-                                  fontSize: 20, fontWeight: FontWeight.bold),
-                            ),
-                            SizedBox(
-                              height: 6,
-                            ),
-                            Text(
-                              "Released - ${favorites[index]["release"] ?? '--'}",
-                              style: TextStyle(
-                                  fontSize: 16, fontWeight: FontWeight.w400),
-                            ),
-                            SizedBox(
-                              height: 12,
-                            ),
-                            Row(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              mainAxisSize: MainAxisSize.max,
-                              children: [
-                                Text(
-                                  "⭐ ${favorites[index]["rating"] ?? '--'}",
-                                  style: TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.w500),
-                                ),
-                                Spacer(),
-                                InkWell(
-                                    onTap: () {
-                                      removeFavorites(favorites[index]);
-                                      ScaffoldMessenger.of(context)
-                                          .showSnackBar(
-                                              SnackBar(content: Text("Movie removed from favorites")));
-                                      setState(() {});
-                                    },
-                                    child: Icon(
-                                      Icons.delete_forever_rounded,
-                                      color: Colors.redAccent,
-                                      size: 30,
-                                    ))
-                              ],
-                            ),
-                          ],
+                return Dismissible(
+                  key: ValueKey(index),
+                  background: Container(
+                    color: Theme.of(context).errorColor,
+                    child: Icon(Icons.delete, color: Colors.white),
+                    alignment: Alignment.centerRight,
+                    padding: EdgeInsets.only(right: 20),
+                    margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+                  ),
+                  direction: DismissDirection.endToStart,
+                  onDismissed: (direction) {
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content: Text("Movie Removed from Favourites")));
+                    setState(() {
+                      removeFavorites(favorites[index]);
+                    });
+                  },
+                  child: Container(
+                    height: 120,
+                    margin: EdgeInsets.all(5),
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(color: Colors.black),
+                    child: Row(
+                      children: [
+                        Image(
+                          image: NetworkImage(
+                              "https://image.tmdb.org/t/p/w500" +
+                                  favorites[index]["posterurl"]),
                         ),
-                      )
-                    ],
+                        SizedBox(
+                          width: 15,
+                        ),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text(
+                                favorites[index]["name"],
+                                style: TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.bold),
+                              ),
+                              SizedBox(
+                                height: 6,
+                              ),
+                              Text(
+                                "Released - ${favorites[index]["release"] ?? '--'}",
+                                style: TextStyle(
+                                    fontSize: 16, fontWeight: FontWeight.w400),
+                              ),
+                              SizedBox(
+                                height: 12,
+                              ),
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                mainAxisSize: MainAxisSize.max,
+                                children: [
+                                  Text(
+                                    "⭐ ${favorites[index]["rating"] ?? '--'}",
+                                    style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w500),
+                                  ),
+                                  Spacer(),
+                                  //InkWell(
+                                  //  onTap: () {
+                                  //  removeFavorites(favorites[index]);
+                                  //ScaffoldMessenger.of(context)
+                                  //  .showSnackBar(
+                                  //    SnackBar(content: Text("Movie removed from favorites")));
+                                  //   setState(() {});
+                                  //},
+                                  //child:
+                                  //Icon(
+                                  //Icons.delete_forever_rounded,
+                                  //color: Colors.redAccent,
+                                  //size: 30,
+                                  //))
+                                ],
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 );
               }),
